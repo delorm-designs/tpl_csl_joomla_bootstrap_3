e1c8c63690c4ef575cb6fa0f0bf80753f97acba4
Rassalon <rpcoomes@gmail.com>
2015-03-29 21:58:22 -0700

    Add support for "Target Window" option in Menu Manager.
    Add support for Joomla's "Target Window" option in the Menu Manager.

--- html/mod_bootstrap_accordionmenu/default.php
+++ html/mod_bootstrap_accordionmenu/default.php
@@ -27,7 +27,30 @@ if (typeof modBootstrapAccordionMenu_hover === "undefined") {
             <?php if(!empty($parent->childs)) : ?>
             <a class="accordion-toggle accordion-parent <?php echo implode(' ', $parent->classes); ?>" data-toggle="collapse" data-parent="#<?php echo $tag_id; ?>" data-href="<?php echo $parent->href; ?>" href="#<?php echo $parent->html_id; ?>"><?php echo $parent->title; ?></a>
             <?php else: ?>
+            <?php
+            switch ($parent->browserNav) :
+            default:
+            case 0:
+            ?>
             <a class="accordion-toggle <?php echo implode(' ', $parent->classes); ?>" href="<?php echo $parent->href; ?>"><?php echo $parent->title; ?></a>
+
+                <?php
+                break;
+            case 1:
+            // _blank
+            ?>
+            <a class="accordion-toggle <?php echo implode(' ', $parent->classes); ?>" href="<?php echo $parent->href; ?>" target="_blank"><?php echo $parent->title; ?></a>
+                <?php
+                break;
+            case 2:
+            // Use JavaScript "window.open"
+            $options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
+            ?>
+            <a class="accordion-toggle <?php echo implode(' ', $parent->classes); ?>" href="<?php echo $parent->href; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options;?>');return false;"><?php echo $parent->title; ?></a>
+                <?php
+                break;
+            endswitch;
+                ?>
             <?php endif; ?>
         </div>
         <?php if(!empty($parent->childs)) : ?>
--- html/mod_bootstrap_accordionmenu/default_submenu.php
+++ html/mod_bootstrap_accordionmenu/default_submenu.php
@@ -14,7 +14,29 @@ defined('_JEXEC') or die;
 <ul>
     <?php foreach($items as $item) : ?>
     <li class="<?php echo implode(' ', $item->classes); ?>">
+        <?php
+            switch ($item->browserNav) :
+            default:
+            case 0:
+        ?>
         <a href="<?php echo $item->href; ?>"><?php echo $item->title; ?></a>
+        <?php
+                break;
+            case 1:
+                // _blank
+        ?>
+        <a href="<?php echo $item->href; ?>" target="_blank"><?php echo $item->title; ?></a>
+        <?php
+                break;
+            case 2:
+                // Use JavaScript "window.open"
+                $options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
+        ?>
+        <a href="<?php echo $item->href; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options;?>');return false;"><?php echo $item->title; ?></a>
+        <?php
+            break;
+            endswitch;
+        ?>
         <?php if(!empty($item->childs)) : ?>
         <?php modBootstrapAccordionMenuHelper::submenu($item->childs); ?>
         <?php endif; ?>
