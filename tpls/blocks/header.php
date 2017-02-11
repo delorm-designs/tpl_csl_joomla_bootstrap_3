c184c065d4af09294030bea21bdfd960c29e64fa
Robert Coomes <rpcoomes@gmail.com>
2017-02-11 09:55:01 -0800

    Modify header positions
    Modify header positions changing header-1 and header-2 to header-left
    and header-right respectivly.  Added a header-bottom for prayer request
    buttons to sit at bottom postion with dynamic height header.

--- tpls/blocks/header.php
+++ tpls/blocks/header.php
@@ -28,40 +28,47 @@ if ($headright = $this->countModules('head-search or languageswitcherload')) {
 <!-- HEADER -->
 <header id="t3-header" class="container t3-header">
 	<div class="row">
-
-        <!-- LOGO -->
-        <div class="col-us-12 col-xs-6 col-sm-6 col-md-4 col-lg-4">
-            <div class="logo-<?php echo $logotype, ($logoimgsm ? ' logo-control' : '') ?>">
-                <a href="<?php echo JURI::base(true) ?>" title="<?php echo strip_tags($sitename) ?>">
-                    <?php if($logotype == 'image'): ?>
-                        <img class="logo-img img-responsive" src="<?php echo JURI::base(true) . '/' . $logoimage ?>" alt="<?php echo strip_tags($sitename) ?>" />
+        <div class="col-md-9">
+            <div class="row">
+                <div class="col-us-12 col-xs-6 col-sm-6 col-md-5">
+                    <div class="logo-<?php echo $logotype, ($logoimgsm ? ' logo-control' : '') ?>">
+                        <a href="<?php echo JURI::base(true) ?>" title="<?php echo strip_tags($sitename) ?>">
+                            <?php if($logotype == 'image'): ?>
+                                <img class="logo-img img-responsive" src="<?php echo JURI::base(true) . '/' . $logoimage ?>" alt="<?php echo strip_tags($sitename) ?>" />
+                            <?php endif ?>
+                            <?php if($logoimgsm) : ?>
+                                <img class="logo-img-sm" src="<?php echo JURI::base(true) . '/' . $logoimgsm ?>" alt="<?php echo strip_tags($sitename) ?>" />
+                            <?php endif ?>
+                            <span><?php echo $sitename ?></span>
+                        </a>
+                        <small class="site-slogan"><?php echo $slogan ?></small>
+                    </div>
+                </div>
+                <div class="col-us-12 col-xs-6 col-sm-6 col-md-7">
+                    <?php if ($this->countModules('header-left')) : ?>
+                        <div class="header-left <?php $this->_c('header-left') ?>">
+                            <jdoc:include type="modules" name="<?php $this->_p('header-left') ?>" style="raw" />
+                        </div>
                     <?php endif ?>
-                    <?php if($logoimgsm) : ?>
-                        <img class="logo-img-sm" src="<?php echo JURI::base(true) . '/' . $logoimgsm ?>" alt="<?php echo strip_tags($sitename) ?>" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <?php if ($this->countModules('header-bottom')) : ?>
+                        <div class="header-left <?php $this->_c('header-bottom') ?>">
+                            <jdoc:include type="modules" name="<?php $this->_p('header-bottom') ?>" style="raw" />
+                        </div>
                     <?php endif ?>
-                    <span><?php echo $sitename ?></span>
-                </a>
-                <small class="site-slogan"><?php echo $slogan ?></small>
+                </div>
             </div>
         </div>
-        <!-- //LOGO -->
-
-        <div class="col-us-12 col-xs-6 col-sm-6 col-md-5 col-lg-5">
-            <?php if ($this->countModules('header-1')) : ?>
-                <div class="header-left <?php $this->_c('header-1') ?>">
-                    <jdoc:include type="modules" name="<?php $this->_p('header-1') ?>" style="raw" />
-                </div>
-            <?php endif ?>
-        </div>
-
         <div class="col-md-3 hidden-sm hidden-xs">
-            <?php if ($this->countModules('header-2')) : ?>
-                <div class="header-right  <?php $this->_c('header-2') ?>">
-                    <jdoc:include type="modules" name="<?php $this->_p('header-2') ?>" style="raw" />
+            <?php if ($this->countModules('header-right')) : ?>
+                <div class="header-right  <?php $this->_c('header-right') ?>">
+                    <jdoc:include type="modules" name="<?php $this->_p('header-right') ?>" style="raw" />
                 </div>
             <?php endif ?>
         </div>
-
 	</div>
 </header>
 <!-- //HEADER -->
